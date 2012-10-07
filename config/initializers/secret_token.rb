20856d2eef7a99108dd58524da8c4e9b97493798
milagros <mirakel1401@gmail.com>
2012-10-07 08:16:30 -0500

    subiendo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pyinformaticos::Application.config.secret_token = 'e7815086c18b1ac3f18b9c38e25655dfb00d568807930ab47b3e7df470b186c8bc2813e873d4b5a4f282cf5559f726339d0026f8e03a9c4d5150150e32ea7c4d'
